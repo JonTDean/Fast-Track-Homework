fe187015cba1dc780f091ecb8e99a74b70adad79
JonTDean <JonathanTomDean@gmail.com>
2020-01-12 02:51:02 +0000

    Initial commit

---
+++ instructions.md
@@ -0,0 +1,9 @@
+Question 1: create a method `greeter` that accepts a name as an argument and prints a greeting of your choice to that name. For instance, if you invoke greeter('Jack') it should put "Hey how's it going Jack??"
+
+Question 2: create a method called yoans_faves that will accept an argument of a motorcycle (string). If the motorcycle is "Harley", it should put "Yoan says: OMG!"; if the motorcycle is "Honda", it should put "Yoan says: Meh." and in any other case it should put "Yoan says: Get me a Harley".
+
+Question 3: create a method `tick_boom` that accepts an integer as an argument. The method will print "tick" as many times as the integer passed in and when it reaches 0, it will print “boom” So for instance, if we passed in 6 it would print `tick tick tick tick tick tick boom`. 
+
+Question 4: create a method `potato_family?` that accepts the array:
+plants = ["potato", "pear", "apple", "yam", "sweet potato"]
+The method will iterate over the array. If the element is a potato, yam or sweet potato, it will print `yes, it belongs to potatos` and it not, it will print `nope, definitely not related to a potato`.
---
+++ main.rb
@@ -0,0 +1,58 @@
+#GREET THE KING
+def greeter(name)
+  return "Hey how's it going #{name}?"
+end
+puts greeter("John")
+
+#YOANS FAVORITE MOTORCYCLE
+def yoans_faves(motorcycle)
+
+  if motorcycle == "Harley"
+    return "Yoan says: OMG!"
+  elsif motorcycle == "Honda"
+   return "Yoan says: Meh."
+  else
+    return "Yoan says: Get me a Harley"
+  end
+
+end
+puts yoans_faves("kawasaki")
+
+#TICK TICK BOOM
+def tick_boom(int)
+    tick = int
+  
+    while tick > 0
+      tick -= 1
+      
+      if tick == 0
+        return "boom"
+      end
+
+      p tick
+    end
+  
+  end
+puts tick_boom(8)
+
+#POTATO FAMILY
+def potato_family(plants)
+    i = 0
+    plantsL = plants.length
+
+    while i < plants.length
+    
+      plantsI = plants[i]
+
+      if plantsI.include?("potato" || "sweet potato")
+        puts "Yes, #{plants[i]} belongs to potatos."
+      elsif plantsI.include?("yam")
+        puts "Yes, #{plants[3]} belongs to potatos."
+      else
+        puts "nope, #{plants[i]} is definitely not related to a potato."
+      end
+
+     i += 1 
+    end
+  end
+  puts potato_family(["potato", "pear", "apple", "yam", "sweet potato"])
